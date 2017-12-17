c0646d738bd0bcbcb07123b400e88b0b93870412
imoh <imohsis@gmail.com>
2017-12-17 06:16:14 +0000

    hope it works now

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,8 @@ import { DataService } from "../data.service";
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+ goals:any;
+ 
   constructor( private route: ActivatedRoute, private router: Router, private _data: DataService ) { 
     this.route.params.subscribe(res =>console.log(res.id));
   }
